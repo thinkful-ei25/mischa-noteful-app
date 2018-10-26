b544356fcf779774280d6e524c2661901ae96677
mischb <mischarberlin@gmail.com>
2018-10-26 12:08:51 -0400

    updated seeddata and db with collection

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 const Folder = require('../models/folder');
+const Tag = require('../models/tags');
 
-const { notes, folders } = require('../db/seed/data');
+const { notes, folders, tags } = require('../db/seed/data');
 
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
@@ -14,7 +15,9 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
     return Promise.all([
       Note.insertMany(notes),
       Folder.insertMany(folders),
-      Folder.createIndexes()
+      Tag.insertMany(tags),
+      Folder.createIndexes(),
+      Tag.createIndexes()
     ]);    
   })
   .then((res) => console.info(`seeded ${res} notes and folders`))
