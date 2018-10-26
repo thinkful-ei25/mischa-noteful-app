789e426a15bb0f2649c350704352baac69b2f8a8
mischb <mischarberlin@gmail.com>
2018-10-26 12:01:35 -0400

    added tags schema

--- db/seed/data.js
+++ db/seed/data.js
@@ -67,4 +67,23 @@ const folders =  [
     'name': 'Work'
   }
 ];
-module.exports = { notes, folders };
+
+const tags = [
+  {
+    '_id': '222222222222222222222200',
+    'name': 'breed'
+  },
+  {
+    '_id': '222222222222222222222201',
+    'name': 'hybrid'
+  },
+  {
+    '_id': '222222222222222222222202',
+    'name': 'domestic'
+  },
+  {
+    '_id': '222222222222222222222203',
+    'name': 'feral'
+  }
+];
+module.exports = { notes, folders, tags };
